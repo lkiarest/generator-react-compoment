8befb8071fc547a5465a59b530f285416bdaf3c7
qintianxiang <rushi_wowen@163.com>
2018-10-18 14:22:09 +0800

    refine configs

--- generators/app/templates/build.config.js
+++ generators/app/templates/build.config.js
@@ -2,5 +2,6 @@ import path from 'path'
 
 export default {
     rootDir: path.resolve(__dirname, 'src'),
-    targetDir: path.resolve(__dirname, 'lib')
+    targetDir: path.resolve(__dirname, 'lib'),
+    themes: ['default', 'green']
 }
